77642c54737cdb8603dc3df23eb2851a814ef709
Anumita <anumita.banerjee@intglobal.com>
2026-04-21 11:40:34 +0530

    refreshed

--- module/knex_connect.js
+++
@@ -1,8 +0,0 @@
-const knexConfig = require('../model/knexfile');
-//initialize knex
-const knex = require('knex')(knexConfig["stable"]);
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-module.exports = knex;
